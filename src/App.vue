596c9629ce2f6725efa8aca53c411a96822b6974
ZweiZhao <ZweiZhao@foxmail.com>
2018-01-10 15:03:19 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template lang="pug">
+  #app {{content}}
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      content: "Hello Vue"
+    };
+  }
+};
+</script>
+
+
+<style lang='less'>
+* {
+  box-sizing: border-box;
+}
+
+html,
+body,
+#app {
+  height: 100%;
+}
+
+body {
+  margin: 0;
+}
+</style>
